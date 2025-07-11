c0f7c6e5323c713794cf95ed0e6752e6b747f057
Ryan Fortner <ryankfortner@gmail.com>
2025-07-11 19:17:35 -0400

    Add driverstation camera stream

--- src/main/java/org/team2059/MaxQ/BuildConstants.java
+++ src/main/java/org/team2059/MaxQ/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "MaxQ";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 1;
-  public static final String GIT_SHA = "b40152030326016ac7b0f3cb799892d4f261981c";
-  public static final String GIT_DATE = "2025-07-11 18:44:38 EDT";
+  public static final int GIT_REVISION = 2;
+  public static final String GIT_SHA = "3c8ab44a2f7c0bfb705ea66086e3bc66909703f3";
+  public static final String GIT_DATE = "2025-07-11 18:52:00 EDT";
   public static final String GIT_BRANCH = "demo";
-  public static final String BUILD_DATE = "2025-07-11 18:50:25 EDT";
-  public static final long BUILD_UNIX_TIME = 1752274225510L;
+  public static final String BUILD_DATE = "2025-07-11 19:09:37 EDT";
+  public static final long BUILD_UNIX_TIME = 1752275377651L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
--- src/main/java/org/team2059/MaxQ/Robot.java
+++ src/main/java/org/team2059/MaxQ/Robot.java
@@ -5,6 +5,7 @@
 
 package org.team2059.MaxQ;
 
+import edu.wpi.first.cameraserver.CameraServer;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
@@ -40,7 +41,7 @@ public class Robot extends LoggedRobot
         Logger.recordMetadata("ProjectName", "MyProject"); // Set a metadata value
 
         if (isReal()) {
-            Logger.addDataReceiver(new WPILOGWriter()); // Log to a USB stick ("/U/logs")
+//            Logger.addDataReceiver(new WPILOGWriter()); // Log to a USB stick ("/U/logs")
             Logger.addDataReceiver(new NT4Publisher()); // Publish data to NetworkTables
         } else {
             setUseTiming(false); // Run as fast as possible
@@ -50,6 +51,8 @@ public class Robot extends LoggedRobot
         }
 
         Logger.start(); // Start logging! No more data receivers, replay sources, or metadata values may be added.
+
+        CameraServer.startAutomaticCapture();
 
         // Instantiate our RobotContainer.  This will perform all our button bindings, and put our
         // autonomous chooser on the dashboard.
